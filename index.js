dfb8a8a46e6cf024650947da3e8507049f6c6877
Marco Rosetti <marco.rosetti@teachat.it>
2018-07-23 18:13:57 +0200

    Added clients, projects and users.

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ module.exports = params => {
   const task = require("./lib/task")(api);
   const timer = require("./lib/timer")(api);
   const projects = require("./lib/projects")(api);
+  const clients = require("./lib/clients")(api);
+  const users = require("./lib/users")(api);
 
-  return { api, tasks, task, timer, projects };
+  return {
+    api,
+    tasks,
+    task,
+    timer,
+    projects,
+    clients,
+    users
+  };
 };
